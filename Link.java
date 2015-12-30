3e45a5f6cb59586b4155c6364ba19e4a37ec4797
MKA11 <mohit.agrawal619@gmail.com>
2015-12-30 15:53:47 +0530

    Create Link.java

---
+++ Link.java
@@ -0,0 +1,14 @@
+
+package texas;
+
+class Link{
+    int a;
+    int b;
+    int weight;
+    Link(int a, int b, int z){
+        this.a = a;
+        this.b = b;
+        this.weight = z;
+    }
+}
+
